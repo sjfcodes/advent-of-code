cf5a1a429d51f7179c25551c5488e73cf81b7395
sjfox <sjfcodes@gmail.com>
2023-11-30 21:00:24 -0800

    wip

---
+++ template/Day_0_x/index.ts
@@ -0,0 +1,46 @@
+import fs from "fs";
+
+const input = fs.readFileSync("template/Day_x:x/input.txt", {
+  encoding: "utf8",
+});
+
+const getFormattedInput = () => {
+  const sorted = input;
+
+  /**
+   * todo:
+   * -
+   */
+
+  return sorted;
+};
+
+const part1 = () => {
+  console.log("part1");
+  const data = getFormattedInput();
+
+  /**
+   * todo:
+   * -
+   */
+
+  const result = data;
+  console.log(result);
+};
+
+const part2 = () => {
+  console.log("part2");
+  const data = getFormattedInput();
+
+  /**
+   * todo:
+   * -
+   */
+
+  const result = data;
+  console.log(result);
+};
+
+console.log(getFormattedInput());
+// part1();
+// part2();
