bbf7abbc20a41eb9ab33d84045e5abcc1921abce
sjfox <sjfcodes@gmail.com>
2023-12-02 12:53:20 -0800

    update template

--- template/index.ts
+++ template/index.ts
@@ -1,24 +1,29 @@
 import fs from "fs";
 import path from "path";
+import util from "util";
+
+const log = (...args: any[]) =>
+  // @ts-ignore
+  console.log(util.inspect(...args, false, null, true /* enable colors */));
 
 const input = fs.readFileSync(path.resolve(__dirname, "./input.txt"), {
   encoding: "utf8",
 });
 
-const part1 = () => {
+const part1 = (input: string) => {
   const result = input;
 
   return result;
 };
 
-const part2 = () => {
+const part2 = (input: string) => {
   const result = input;
 
   return result;
 };
 
-console.log("part1");
-console.log(part1()); // answer
+log("part1");
+log(part1(input)); // answer
 
-console.log("part2");
-console.log(part2()); // answer
+log("part2");
+log(part2(input)); // answer
