46c57ffc9b61ecc0f57833ba17b9cd267f4273c3
sjfox <sjfcodes@gmail.com>
2023-12-02 10:05:51 -0800

    init day 2

---
+++ events/2023/Day 2: Cube Conundrum/index.ts
@@ -0,0 +1,24 @@
+import fs from "fs";
+import path from "path";
+
+const input = fs.readFileSync(path.resolve(__dirname, "./input.txt"), {
+  encoding: "utf8",
+});
+
+const part1 = () => {
+  const result = input;
+
+  return result;
+};
+
+const part2 = () => {
+  const result = input;
+
+  return result;
+};
+
+console.log("part1");
+console.log(part1()); // answer
+
+console.log("part2");
+console.log(part2()); // answer
